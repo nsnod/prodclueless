3fb479a21477b126c84beee16a0bad7ba946a2e5
nsnod001 <nsnod001@ucr.edu>
2024-11-21 11:03:58 -0800

    Links and more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ function App() {
             <img src="middle.png" className="layer" data-speed="0.4" alt="middle" />
             <img src="secondtrees.png" className="layer" data-speed="0.5" alt="second trees" />
             <img src="lowertrees.png" className="layer" data-speed="0.6" alt="lower trees" />
-            <img src="clueless.png" className="layer" data-speed="0.8" alt="clueless" />
+            <img src="clueless.png" className="layer" data-speed="0.7" alt="clueless" />
           </div>
         </div>
         <div className="whiteCover"></div>
@@ -62,25 +62,25 @@ function App() {
         </p>
 
         <div className="socialIcons">
-          <a href="https://www.spotify.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://open.spotify.com/artist/6JsakuHNJCFmLAVvhpjHLz?si=aDVrMJrMTkqO0aBn-fMMfQ&utm_medium=share&utm_source=linktree&nd=1&dlsi=76b2aeca904d48ee" target="_blank" rel="noopener noreferrer" className="social-icon">
             <img src="spotify.png" alt="Spotify" />
           </a>
-          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://www.youtube.com/@prodclueless" target="_blank" rel="noopener noreferrer" className="social-icon">
             <img src="youtube.png" alt="YouTube" />
           </a>
-          <a href="https://www.beatstars.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://www.beatstars.com/prodclueless" target="_blank" rel="noopener noreferrer" className="social-icon">
             <img src="beatstars.png" alt="BeatStars" />
           </a>
-          <a href="https://www.soundcloud.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://soundcloud.com/clueless27" target="_blank" rel="noopener noreferrer" className="social-icon">
             <img src="soundcloud.png" alt="SoundCloud" />
           </a>
-          <a href="https://www.fiverr.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://www.fiverr.com/prodclueless/mix-and-produce-your-music-with-a-professional-sound?utm_campaign=gigs_show&utm_medium=shared&utm_source=copy_link&utm_term=b4ebbk" target="_blank" rel="noopener noreferrer" className="social-icon">
             <img src="fiver.png" alt="Fiverr" />
           </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://www.instagram.com/prodclueless/" target="_blank" rel="noopener noreferrer" className="social-icon">
             <img src="instagram.png" alt="Instagram" />
           </a>
-          <a href="https://www.patreon.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://www.patreon.com/prodcluelesspacks" target="_blank" rel="noopener noreferrer" className="social-icon">
             <img src="patreon.png" alt="Patreon" />
           </a>
         </div>
